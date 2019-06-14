a5278f3610543e5942bb1fe9198a01987e1648e4
Tech AITPL <tech@aitpl.org>
2019-06-14 15:57:46 +0530

    3rd Commit

--- api/MasterData/setMasterData.php
+++ api/MasterData/setMasterData.php
@@ -19,7 +19,7 @@ if(
     !empty($data->state) &&
     !empty($data->countryId)
 ){
-    if($setMaster->updateState($data->state,$data->countryId))
+    if($setMaster->addState($data->state,$data->countryId))
         echo json_encode("State updated successfully");
     else
         echo json_encode("Update Unsuccessful");
@@ -29,8 +29,62 @@ if(
 if(
     !empty($data->roleType)
 ){
-    if($setMaster->userRole($data->roleType))
-        echo json_encode("State updated successfully");
+    if($setMaster->addUserRole($data->roleType))
+        echo json_encode("Roletype updated successfully");
     else
         echo json_encode("Update Unsuccessful");
+}
+
+if(
+    !empty($data->amenity)
+){
+    if($setMaster->addAmenity($data->amenity))
+        echo json_encode("Amenity uploaded successfully");
+    else
+        echo json_encode("Update Unsuccessful");
+}
+
+if(
+    !empty($data->documentName)
+){
+    if($setMaster->addDocumentType($data->documentName))
+        echo json_encode("Document uploaded successfully");
+    else
+        echo json_encode("Update Unsuccessful");
+}
+
+if(
+    !empty($data->propertyType)
+){
+    if($setMaster->addPropertyType($data->propertyType))
+        echo json_encode("Property Type is inserted successfully");
+    else
+        echo json_encode("cannot be inserted");
+}
+
+if(
+    !empty($data->configurationType)
+){
+    if($setMaster->addConfigurationType($data->configurationType))
+        echo json_encode("Configuration Type is inserted successfully");
+    else
+        echo json_encode("cannot be inserted");
+}
+
+if(
+!empty($data->socialMediaName)
+){
+    if($setMaster->addSocialMedia($data->socialMediaName))
+        echo json_encode("Social Media Type is inserted successfully");
+    else
+        echo json_encode("cannot be inserted");
+}
+
+if(
+    !empty($data->unitName)
+){
+    if($setMaster->addUnit($data->unitName))
+        echo json_encode("Unit Type is inserted successfully");
+    else
+        echo json_encode("cannot be inserted");
 }
--- api/MasterData/updateMasterData.php
+++ api/MasterData/updateMasterData.php
@@ -5,11 +5,19 @@ class UpdateMasterData
     private $conn;
     private $stateTable = "state" ;
     private $roleTable = "role" ;
+    private $amenitiesTable = "amenities" ;
+    private $documentTypeTable = "documentType" ;
+    private $configurationTable = "configuration" ;
+    private $propertyTypeTable = "propertyType" ;
+    private $socialMediaTable = "socialMedia";
+    private $unitTable = "unit";
     public function __construct($db)
     {
         $this->conn = $db;
     }
-    function updateState($state,$countryId)
+
+    //user related
+    function addState($state,$countryId)
     {
         $query = "INSERT INTO $this->stateTable(state , countryId) VALUES (:state , :countryId)";
         $stmt= $this->conn->prepare( $query );
@@ -23,15 +31,115 @@ class UpdateMasterData
             return false;
     }
 
-    function userRole($roleType)
+    function addCity($city,$stateId)
+    {
+        $query4 = "INSERT INTO $this->cityTable(city,stateId) VALUES (:city,:stateId)";
+        $stmt4 = $this->conn->prepare($query4);
+        $stmt4->bindParam(":city", $city);
+        $stmt4->bindParam(":stateId", $stateId);
+        if($stmt4->execute())
+            return true;
+        else
+            return false ;
+    }
+
+    function addPincode($pincode , $cityId)
+    {
+        $query = "INSERT INTO $this->pincodeTable(pincode,cityId) VALUES (:pincode,:cityId)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":pincode", $pincode);
+        $stmt->bindParam(":cityId", $cityId);
+        if($stmt->execute())
+            return true;
+        else
+            return false ;
+    }
+
+    //property related
+    function addUserRole($roleType)
     {
         $query = "INSERT INTO $this->roleTable (roleType) values (:roleType)" ;
         $stmt = $this->conn->prepare($query) ;
         $stmt->bindParam(":roleType" , $roleType ) ;
         if($stmt->execute())
+            return true;
+        else
+            return false;
+
+    }
+
+    function addAmenity($amenity)
+    {
+        $query = "INSERT INTO $this->amenitiesTable(amenity) values (:amenity)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":amenity" , $amenity ) ;
+        if($stmt->execute())
+            return true;
+        else
+            return false;
+
+    }
+
+    function addDocumentType($documentName)
+    {
+        $query = "INSERT INTO $this->documentTypeTable (documentName) values (:documentName)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":documentName" , $documentName ) ;
+        if($stmt->execute())
+            return true;
+        else
+            return false;
+
+    }
+
+    function addPropertyType($propertyType)
+    {
+        $query = "INSERT INTO $this->propertyTypeTable (propertyType) VALUES (:propertyType)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":propertyType" , $propertyType ) ;
+        if($stmt->execute())
             return true ;
         else
             return false ;
+    }
+
+    //inserting new configuration
+    function addConfigurationType($configurationType)
+    {
+        $query = "INSERT INTO $this->configurationTable (configurationType) VALUES (:configurationType)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":configurationType" , $configurationType ) ;
+        if($stmt->execute())
+            return true ;
+        else
+            return false;
+    }
+
+    //inserting social media
+    function addSocialMedia($socialMediaName)
+    {
+        $query = "INSERT INTO $this->socialMediaTable (socialMediaName) VALUES (:socialMediaName)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":socialMediaName" , $socialMediaName ) ;
+        if($stmt->execute())
+            return true ;
+        else
+            return false;
 
     }
+
+    //inserting unit
+    function addUnit($unitName)
+    {
+        $query = "INSERT INTO $this->unitTable (unitName) VALUES (:unitName)" ;
+        $stmt = $this->conn->prepare($query) ;
+        $stmt->bindParam(":unitName" , $unitName ) ;
+        if($stmt->execute())
+            return true ;
+        else
+            return false;
+    }
+
+
+
 }
